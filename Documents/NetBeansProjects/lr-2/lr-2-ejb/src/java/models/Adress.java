239c1cf161ca4ad609dfe76506567da41ae3026e
ENAstya <hzhz.xd@yandex.ru>
2017-04-16 14:09:49 +0300

    start

---
+++ Documents/NetBeansProjects/lr-2/lr-2-ejb/src/java/models/Adress.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Настя
+ */
+@Entity
+public class Adress implements Serializable {
+
+    @OneToMany(mappedBy = "adress")
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String street;
+    private String house;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Adress)) {
+            return false;
+        }
+        Adress other = (Adress) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Adress[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the street
+     */
+    public String getAdress() {
+        return getStreet();
+    }
+
+    /**
+     * @param adress the street to set
+     */
+    public void setAdress(String adress) {
+        this.setStreet(adress);
+    }
+
+    /**
+     * @return the street
+     */
+    public String getStreet() {
+        return street;
+    }
+
+    /**
+     * @param street the street to set
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+     * @return the house
+     */
+    public String getHouse() {
+        return house;
+    }
+
+    /**
+     * @param house the house to set
+     */
+    public void setHouse(String house) {
+        this.house = house;
+    }
+    
+}
---
+++ Documents/NetBeansProjects/lr-2/lr-2-ejb/src/java/models/Client.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Настя
+ */
+@Entity
+public class Client implements Serializable {
+
+    @ManyToMany(mappedBy = "clients")
+    private List<Order> orders;
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fio;
+    private String phone;
+    @ManyToOne
+    private Adress adress;
+    @OneToOne
+    private Passport passport;
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (getId() != null ? getId().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Client)) {
+            return false;
+        }
+        Client other = (Client) object;
+        if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Client[ id=" + getId() + " ]";
+    }
+
+    /**
+     * @return the fio
+     */
+    public String getFio() {
+        return fio;
+    }
+
+    /**
+     * @param fio the fio to set
+     */
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the adress
+     */
+    public Adress getAdress() {
+        return adress;
+    }
+
+    /**
+     * @param adress the adress to set
+     */
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+
+    /**
+     * @return the passport
+     */
+    public Passport getPassport() {
+        return passport;
+    }
+
+    /**
+     * @param passport the passport to set
+     */
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+    /**
+     * @return the orders
+     */
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    /**
+     * @param orders the orders to set
+     */
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+    
+}
